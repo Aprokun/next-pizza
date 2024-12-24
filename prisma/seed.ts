476708acbcf1c882be6ace86936e11d0b4f3e22f
Dmitriy Mashurov <lagrik33@gmail.com>
2024-12-25 02:30:37 +0300

    added creating carts and cart items in seed for prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -318,6 +318,32 @@ async function up() {
 			generateProductItem({ productId: 17 }),
 		],
 	});
+
+	await prisma.cart.createMany({
+		data: [
+			{
+				userId: 1,
+				totalAmount: 0,
+				tokenId: '11111',
+			},
+			{
+				userId: 2,
+				totalAmount: 0,
+				tokenId: '222222',
+			},
+		],
+	});
+
+	await prisma.cartItem.create({
+		data: {
+			productItemId: 1,
+			cartId: 1,
+			quantity: 2,
+			ingredients: {
+				connect: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+			},
+		},
+	});
 }
 
 async function down() {
