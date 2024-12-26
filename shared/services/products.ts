3ac38dbc39b3d3b3d565d3ddcf79ef683a2abb48
Dmitriy Mashurov <lagrik33@gmail.com>
2024-12-27 01:39:26 +0300

    refactoring

--- shared/services/products.ts
+++ shared/services/products.ts
@@ -1,6 +1,6 @@
-import { axiosInstance } from '@/services/instance';
+import { axiosInstance } from '@/shared/services/instance';
 import { Product } from '@prisma/client';
-import { ApiRoutes } from '@/services/constants';
+import { ApiRoutes } from '@/shared/services/constants';
 
 export const search = async (query: string) => {
 	const { data } = await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS_ROUTE, {
