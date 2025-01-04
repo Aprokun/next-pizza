766ea42baeffcf389182d7739e47547ac27f01a5
Dmitriy Mashurov <lagrik33@gmail.com>
2025-01-04 04:45:46 +0300

    added common hook for cart logic

--- shared/hooks/index.ts
+++ shared/hooks/index.ts
@@ -1,3 +1,6 @@
 export { useFilters } from './useFilters';
 export { useIngredients } from './useIngredients';
 export { useQueryFilters } from './useQueryFilters';
+export { useFilterIngredients } from './useFilterIngredients';
+export { usePizzaOptions } from './usePizzaOptions';
+export { useCart } from './useCart';
