c521ca0515862ae186194873fe7ef6daba2159bb
Dmitriy Mashurov <lagrik33@gmail.com>
2025-01-04 04:45:19 +0300

    added nullable for type and size

--- shared/lib/get-cart-item-details.ts
+++ shared/lib/get-cart-item-details.ts
@@ -1,11 +1,10 @@
 import { mapPizzaType, PizzaSize, PizzaType } from '@/shared/constants/pizza';
-import { Ingredient } from '@prisma/client';
 import { CartStateItem } from '@/shared/lib/get-cart-details';
 
 export const getCartItemDetails = (
 	ingredients: CartStateItem['ingredients'],
-	pizzaType: PizzaType,
-	pizzaSize: PizzaSize,
+	pizzaType?: PizzaType,
+	pizzaSize?: PizzaSize,
 ) => {
 	const details = [];
 
